2182915d5fef315bea5005ad52cc560767f0d1cb
darseen <darseen.dev@gmail.com>
2025-12-23 03:13:45 +0300

    db setup

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,9 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db";
 
-export const auth = betterAuth({});
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+});
